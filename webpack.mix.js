86167fe0488a1efd9ca6220c381bae49c7569069
k-yasuda <yasuda.keisuke.1194@colopl.co.jp>
2020-03-06 02:09:09 +0900

    now coreui-react is working on laravel

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,4 @@
-const path          = require('path')
-const mix           = require('laravel-mix')
-const webpack       = require('webpack')
-const { version }   = require('./package.json')
-const WebpackBar    = require('webpackbar')
-const OfflinePlugin = require('offline-plugin')
-
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -18,53 +12,16 @@ const OfflinePlugin = require('offline-plugin')
  */
 
 mix.react('resources/js/app.js', 'public/js')
-mix.sass('resources/sass/app.scss', 'public/css')
-
-mix.webpackConfig({
-  devServer: { disableHostCheck: true },
-  resolve  : {
-    alias: {
-      '@'         : path.resolve(__dirname, 'resources/js/coreui/'),
-      'static'    : path.resolve(__dirname, 'resources/static/'),
-    },
-  },
-  plugins: [
-    new WebpackBar({ profile: true }),
-    new webpack.DefinePlugin({ __VERSION: JSON.stringify(version) }),
-    new OfflinePlugin({
-      publicPath      : '/',
-      appShell        : '/',
-      responseStrategy: 'network-first',
-      externals       : [
-        '/',
-        '/manifest.json',
-        '/favicon.png',
+    .sass('resources/sass/app.scss', 'public/css')
+    .babelConfig({
+      "presets": [
+        "@babel/preset-env"
       ],
-      ServiceWorker: {
-        entry : path.resolve(__dirname, 'resources/js/sw.js'),
-        output: 'sw.js',
-        minify: mix.inProduction(),
-      },
-    }),
-  ],
-})
+      "plugins": [
+        "@babel/plugin-proposal-class-properties"
+      ],
+    });
 
-mix.extract([
-  'axios',
-  'bootstrap',
-  'jquery',
-  'lodash',
-])
-
-mix.options({
-  clearConsole: false,
-  terser: { terserOptions: { parallel: true } },
-})
-
-if (mix.inProduction())
-  mix.version()
-else
-  mix.sourceMaps()
-
-if (process.platform === 'darwin')
-  mix.disableNotifications()
+if (mix.inProduction()) {
+  mix.version();
+}
